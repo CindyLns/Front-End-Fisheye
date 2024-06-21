243f50c89cdef1b5f117d536da9e2d3612196312
Cindy Lionis <cindy.lionis@gmail.com>
2024-06-21 11:02:05 +0200

    importation des datas

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,38 +1,46 @@
     async function getPhotographers() {
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+        const reponse = await fetch("photographers.json");
+        const data = await reponse.json()
+
+        const photographers = data.photographers;
+
         // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        return photographers;
     }
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
 
         photographers.forEach((photographer) => {
-            const photographerModel = photographerTemplate(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
+            const dataElement = document.createElement("article");
+    
+            const imageElement = document.createElement("img");
+            imageElement.src = photographer.image;
+            imageElement.alt = photographer.name;
+            const nomElement = document.createElement("h2");
+            nomElement.innerText = photographer.nom;
+            const idElement = document.createElement("p");
+            idElement.innerText = photographer.id;
+            const cityElement = document.createElement("p");
+            cityElement.innerText = photographer.city;
+            const countryElement = document.createElement("p");
+            countryElement.innerText = photographer.country;
+            const taglineElement = document.createElement("p");
+            taglineElement.innerText = photographer.tagline;
+            const priceElement = document.createElement("p");
+            priceElement.innerText = `${photographer.price} €`;
+
+            photographersSection.appendChild(dataElement);
+
+            dataElement.appendChild(imageElement);
+            dataElement.appendChild(nomElement);
+            dataElement.appendChild(idElement);
+            dataElement.appendChild(cityElement);
+            dataElement.appendChild(countryElement);
+            dataElement.appendChild(taglineElement);
+            dataElement.appendChild(priceElement);
         });
     }
 
